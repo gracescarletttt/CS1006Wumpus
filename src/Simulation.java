d5041c674f8bf677d8ca86cfee247917d73d6cf6
lphin9 <127103517+lphin9@users.noreply.github.com>
2023-04-09 06:26:19 +0100

    error fixes, checks and actions for things in caves, stopped player being true for all previously stood on caves

--- src/Simulation.java
+++ src/Simulation.java
@@ -2,14 +2,21 @@ import java.util.Random;
 
 public class Simulation {
     private Cave[][] caves = new Cave[20][20];
+    private Player me = new Player();
 
     public Simulation(){
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 20; j++) {
-                this.caves[i][j] = new Cave(i,j);
+        for (int y = 0; y < 20; y++) {
+            for (int x = 0; x < 20; x++) {
+                this.caves[y][x] = new Cave(y,x);
             }
         }
+        
     }
+
+
+    // public void setPlayerObj(Player me){
+    //      this.me = me;
+    // }
 
     public int getRandomNo(){
         Random rand = new Random();
@@ -20,24 +27,24 @@ public class Simulation {
         for(int i = 0; i<thisMany; i++){
             int x = getRandomNo();
             int y = getRandomNo();
-            if(this.caves[x][y].getCellState()){
+            if(this.caves[y][x].getCellState()){
                 i--;
             }
             else{
                 if(beingSet.equalsIgnoreCase("treasure")){
-                    this.caves[x][y].setTreasure(true);
+                    this.caves[y][x].setTreasure(true);
                 }
                 else if(beingSet.equalsIgnoreCase("hole")){
-                    this.caves[x][y].setHole(true);
+                    this.caves[y][x].setHole(true);
                 }
                 else if(beingSet.equalsIgnoreCase("exit")){
-                    this.caves[x][y].setExit(true);
+                    this.caves[y][x].setExit(true);
                 }
                 else if(beingSet.equalsIgnoreCase("bats")){
-                    this.caves[x][y].setBat(true);
+                    this.caves[y][x].setBat(true);
                 }
                 else if(beingSet.equalsIgnoreCase("wumpus")){
-                    this.caves[x][y].setWumpus(true);
+                    this.caves[y][x].setWumpus(true);
                 }
             }
         }
@@ -65,34 +72,82 @@ public class Simulation {
     public void setWumpus(int thisMany){
         setting("wumpus", thisMany);
     }
-     public void setPlayer(int x, int y){
-          this.caves[x][y].setPlayer(true);
+    public void setPlayer(int y, int x){
+        this.caves[y][x].setPlayer(true);
     }
+    public void removePlayer(int y, int x){
+        this.caves[y][x].setPlayer(false);
+    }
+    public void setRoute(int y, int x){
+        this.caves[y][x].setRoute(true);
+    }
+
 
     public Cave[][] getCaves() {
         return this.caves;
     }
 
+
+
+    public boolean checkHole(int y, int x){
+        if(this.caves[y][x].getHole() == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean checkTreasure(int y, int x){
+        if(this.caves[y][x].getTreasure() == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean checkBats(int y, int x){
+        if(this.caves[y][x].getBat() == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean checkExit(int y, int x){
+        if(this.caves[y][x].getExit() == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+
     public void displayBoard(){
-        for(int i = 0; i < 20; i++){
-            for(int j = 0; j < 20; j++){
-                if(this.caves[i][j].getPlayer()){
+        for(int y = 0; y < 20; y++){
+            for(int x = 0; x < 20; x++){
+                if(this.caves[y][x].getPlayer()){
                     System.out.print("P");
                 }
-                else if(this.caves[i][j].getBat()){
+                else if(this.caves[y][x].getBat()){
                     System.out.print("B");
                 }
-                else if(this.caves[i][j].getExit()){
+                else if(this.caves[y][x].getExit()){
                     System.out.print("E");
                 }
-                else if(this.caves[i][j].getTreasure()){
+                else if(this.caves[y][x].getTreasure()){
                     System.out.print("T");
                 }
-                else if(this.caves[i][j].getHole()){
+                else if(this.caves[y][x].getHole()){
                     System.out.print("H");
                 }
-                else if(this.caves[i][j].getWumpus()){
+                else if(this.caves[y][x].getWumpus()){
                     System.out.print("W");
+
+                }
+                else if(this.caves[y][x].getRoute()){
+                    System.out.print("o");
                 }
                 else{
                     System.out.print(".");
@@ -103,3 +158,4 @@ public class Simulation {
         }
     }
 }
+
